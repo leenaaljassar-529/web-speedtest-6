31eae72dc0b261aae55cf3670bf65d896c796e21
leenaaljassar-529 <66418005+leenaaljassar-529@users.noreply.github.com>
2020-07-04 17:09:22 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,23 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+  if (greet != name) {
+    console.log("Hello");
+  } else {
+    console.log("hello" + name);
+  }
+}
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+  if (n % 2 == 0) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
